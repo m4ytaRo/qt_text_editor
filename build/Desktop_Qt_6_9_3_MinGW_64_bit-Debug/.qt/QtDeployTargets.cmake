d1bd442d0497fa3a15cd808a42dbeecb883b24ba
m4ytaRo <maytarosheejsh@gmail.com>
2025-10-15 21:55:31 +0300

    Initial commit

---
+++ build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_qt_TextEditor_FILE D:/QT_PROJ/qt_TextEditor/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/qt_TextEditor.exe)
+set(__QT_DEPLOY_TARGET_qt_TextEditor_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_qt_TextEditor_RUNTIME_DLLS E:/QT/6.9.3/mingw_64/bin/Qt6Widgets.dll;E:/QT/6.9.3/mingw_64/bin/Qt6Gui.dll;E:/QT/6.9.3/mingw_64/bin/Qt6Core.dll)
